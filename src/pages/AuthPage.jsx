34f4a49b398a51af99eec62fdc5a8b805e7369bc
Dickson_Goh <dicksongoh.biz@gmail.com>
2025-01-09 10:09:17 +0800

    Add facebook login

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -83,6 +83,9 @@ export default function AuthPage() {
                     <Button className="rounded-pill" variant="outline-dark">
                         <i className="bi bi-apple"> Sign up with Apple</i>
                     </Button>
+                    <Button className="rounded-pill" variant="outline-dark">
+                        <i className="bi bi-facebook"> Sign up with Facebook</i>
+                    </Button>
                     <Button className="rounded-pill" onClick={handleShowSignUp}>
                         Create an account
                     </Button>
